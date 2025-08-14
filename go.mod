ba271fbb349e9cf656803e2e31037670949e3d13
Prabhat Shukla <prabhat@DESKTOP-0N74OTG.localdomain>
2025-08-14 20:39:45 +0530

    new thing

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/PRABHAT1SHUKLA/ping-analyzer
+
+go 1.25.0
+
+require github.com/guptarohit/asciigraph v0.7.3 // indirect
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+	defaultTarget        = "google.com" // Default ping target
+	defaultCount         = 10           // Default number of pings (0 for infinite)
+	defaultInterval      = 1            // Seconds between pings
+	highLatencyThreshold = 100          // ms; alert if above this
+)
